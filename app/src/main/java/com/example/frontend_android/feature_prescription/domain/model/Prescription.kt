1129ed97f9a89a8c2e313b309006773fb406b58e
MathisMchd <michenaudmathis@gmail.com>
2023-10-21 15:52:24 +0200

    App Structure

---
+++ app/src/main/java/com/example/frontend_android/feature_prescription/domain/model/Prescription.kt
@@ -0,0 +1,12 @@
+package com.example.frontend_android.feature_prescription.domain.model
+
+
+data class Prescription(
+    val id: Int,
+    val titre: String,
+    val contenu: String
+        ) {
+
+
+
+}
