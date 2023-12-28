f77e54ad5cb4b3f67fc567584089693784ea42d2
MathisMchd <michenaudmathis@gmail.com>
2023-12-28 16:17:33 +0100

    fix navigation

--- app/src/main/java/com/example/frontend_android/pages/user/ViewUser.kt
+++ app/src/main/java/com/example/frontend_android/pages/user/ViewUser.kt
@@ -3,12 +3,18 @@ package com.example.frontend_android.pages.user
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.example.frontend_android.layout.PageLayout
 
 @Composable
 fun ViewUser(
     navController: NavController
 ) {
-    
-    Text(text = "Page utilisateur")
-    
+    PageLayout(
+        title = "Mes Informations",
+        canGoBack = false,
+        navController = navController
+    ) {
+        Text(text = "Page utilisateur")
+    }
+
 }
