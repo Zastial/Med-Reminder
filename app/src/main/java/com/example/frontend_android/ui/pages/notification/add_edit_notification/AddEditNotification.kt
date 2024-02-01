595bc85498aa345e30079cff8356928282d47900
Mathis MICHENAUD <E211769J@u-inf-j-e113-10.ad.iut-nantes.univ-nantes.prive>
2024-02-01 16:18:01 +0100

    ui notification

--- app/src/main/java/com/example/frontend_android/ui/pages/notification/add_edit_notification/AddEditNotification.kt
+++ app/src/main/java/com/example/frontend_android/ui/pages/notification/add_edit_notification/AddEditNotification.kt
@@ -1,19 +1,24 @@
 package com.example.frontend_android.ui.pages.notification.add_edit_notification
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.frontend_android.ui.components.buttons.BtnContinue
 import com.example.frontend_android.ui.components.layout.TopBar
 import com.example.frontend_android.ui.theme.MedreminderTheme
 import kotlinx.coroutines.flow.collectLatest
@@ -25,7 +30,7 @@ fun AddEditNotificationScreen(
 ) {
     val state = viewModel.state.value
 
-    val scaffoldState = rememberScaffoldState()
+
 
 
     // observeur permettant de récupéré les evenement du viewModel
@@ -47,26 +52,44 @@ fun AddEditNotificationScreen(
 
     Scaffold(
         modifier = Modifier.fillMaxHeight(),
-        scaffoldState = scaffoldState,
-        topBar = { TopBar(navController= navController, canGoBack = true, title = "Ajouter une alarme") },
+        topBar = { TopBar(navController= navController, canGoBack = true, title = "Ajouter une alarme")},
     ) {
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it)
+                .padding(16.dp)
         ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it),
+                horizontalArrangement = Arrangement.Center,
+            ) {
 
-            BasicTextField(
-                value = state.hour,
-                onValueChange = { viewModel.onEvent(AddEditNotificationEvent.EnteredHour(it))}
-            )
+                TextField(
+                    modifier = Modifier.width(100.dp),
+                    value = state.hour,
+                    placeholder = { Text(text="00") },
+                    onValueChange = { viewModel.onEvent(AddEditNotificationEvent.EnteredHour(it))}
+                )
 
-            BasicTextField(
-                value = state.minutes,
-                onValueChange = { viewModel.onEvent(AddEditNotificationEvent.EnteredMinute(it))}
+                Text(text = ":")
+
+                TextField(
+                    modifier = Modifier.width(100.dp),
+                    value = state.minutes,
+                    placeholder = { Text(text="00") },
+                    onValueChange = { viewModel.onEvent(AddEditNotificationEvent.EnteredMinute(it))}
+                )
+            }
+
+            BtnContinue(
+                actionText = "Sauvegarder",
+                onClick = { viewModel.onEvent(AddEditNotificationEvent.SaveNotification) }
             )
         }
+
     }
 
 }
