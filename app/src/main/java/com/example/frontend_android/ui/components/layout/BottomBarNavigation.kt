8bce978a96b0c0ee359ba9e31069a5796173e8c9
E211861J <E211861J@etu.univ-nantes.fr>
2024-02-02 17:50:19 +0100

    feat: begin changing color scheme

--- app/src/main/java/com/example/frontend_android/ui/components/layout/BottomBarNavigation.kt
+++ app/src/main/java/com/example/frontend_android/ui/components/layout/BottomBarNavigation.kt
@@ -1,16 +1,13 @@
 package com.example.frontend_android.ui.components.layout
 
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -22,7 +19,6 @@ import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.frontend_android.navigation.RootScreen
-import com.example.frontend_android.ui.theme.White
 
 val bottomNavigationItem: List<RootScreen> = listOf(
     RootScreen.prescription,
@@ -43,19 +39,20 @@ fun BottomBarNavigation(
         val currentDestination = navBackStackEntry?.destination
         bottomNavigationItem.forEach { screen ->
             val isSelected = isSelected(currentDestination, screen.route)
-            val colorNavItem = if (isSelected) MaterialTheme.colorScheme.primary else Color.Black
+            // TODO
+            // val colorNavItem = if (isSelected) MaterialTheme.colorScheme.primary else Color.Black
             NavigationBarItem(
                 icon = {
                     Icon(
                         imageVector = ImageVector.vectorResource(id = screen.icon),
                         contentDescription = null,
-                        tint = colorNavItem
+                        // tint = colorNavItem
                     )
                 },
                 label = {
                     Text(
                         text = stringResource(id = screen.ressourceID),
-                        color = colorNavItem,
+                        // color = colorNavItem,
                         fontSize = 10.sp
                     )
                 },
