023a675d32e00a8d5602725dbc60bdaf9c737f0e
E21B036Z <alexandre.carol@etu.univ-nantes.fr>
2024-03-25 15:24:43 +0100

    Fix material3 version + Implement generic Loading

--- app/src/main/java/com/example/frontend_android/ui/components/cards/SideEffectCard.kt
+++ app/src/main/java/com/example/frontend_android/ui/components/cards/SideEffectCard.kt
@@ -1,20 +1,23 @@
 package com.example.frontend_android.ui.components.cards
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +28,6 @@ import java.time.LocalDate
 @Composable
 fun SideEffectCard(
     sideEffect: SideEffect,
-    modifier: Modifier = Modifier,
 ) {
     Card(
         modifier = Modifier
@@ -50,19 +52,25 @@ fun SideEffectCard(
                 overflow = TextOverflow.Ellipsis
             )
             Spacer(modifier = Modifier.size(12.dp))
-            OutlinedTextField(
+            Text(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(200.dp),
-                value = sideEffect.description,
-                onValueChange = {},
-                readOnly = true,
+                    .height(100.dp)
+                    .border(
+                        BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                        shape = RoundedCornerShape(4.dp)
+                    )
+                    .padding(all = 8.dp),
+                text = sideEffect.description,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Justify,
+                maxLines = 5,
             )
         }
     }
 }
 
-@Preview(showBackground = true, )
+@Preview(showBackground = true)
 @Composable
 fun SideEffectPreview() {
     SideEffectCard(
